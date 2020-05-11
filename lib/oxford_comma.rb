18b4c32b1d332ad3cbfa8b0d60c6c21d8d823dab
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-11 17:41:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,15 @@
 def oxford_comma(array)
-  ["fiddleheads","okra","kohlrabi"]
-  ["fiddleheads","okra","kohlrabi"].split(",")
+
 end
+
+
+
+#if stock < 1
+# puts "Sorry we are out of stock!"
+#end 
+
+
+## declaring array 
+# b = [1, 4, 1, 1, 88, 9]  
+
+#puts "length() method form : #{b.length()}\n\n"
