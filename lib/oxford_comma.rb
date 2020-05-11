e1fa54b97cb2186c64de240e1ab620df04085a6c
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-11 18:54:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,10 @@ def oxford_comma(array)
 
   if array.length == 1
     return array.join #when called on a array, will convert it into a string.
-  end
-  if array.length == 2
+  elsif array.length == 2
     return array.join(" and ") #Spaces keep the words seperated
+  else
+    array [0..-2].join(", ") + ", and "
   end
 
 end
