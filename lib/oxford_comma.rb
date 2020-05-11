56052dfd0a141cbc91e13760226f1e08ed52e21a
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-11 17:41:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,10 +6,10 @@ end
 
 #if stock < 1
 # puts "Sorry we are out of stock!"
-#end 
+#end
 
 
-## declaring array 
-# b = [1, 4, 1, 1, 88, 9]  
+## declaring array
+# b = [1, 4, 1, 1, 88, 9]
 
 #puts "length() method form : #{b.length()}\n\n"
